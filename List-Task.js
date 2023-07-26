2e02b47635e134aaafed2196f551d3304378e68d
Lavanya <108626847+Lavanya012345@users.noreply.github.com>
2023-07-26 14:23:24 +0530

    Update List-Task.js

--- List-Task.js
+++ List-Task.js
@@ -5,27 +5,27 @@ let uniqueListEl1 = [], uniqueListEl2 = [], commonListEl = [];
 
 
 for (let i = 0; i < listElement1.length; i++) {
-    let found = false;
+    let is_false = false;
     for (let j = 0; j < listElement2.length; j++) {
         if (listElement1[i] === listElement2[j]) {
-            found = true;
+            is_false = true;
             break;
         }
     }
-    if (!found) uniqueListEl1.push(listElement1[i]);
+    if (!is_false) uniqueListEl1.push(listElement1[i]);
 }
 
 console.log(uniqueListEl1);
 
 for (let i = 0; i < listElement2.length; i++) {
-    let found = false;
+    let is_false = false;
     for (let j = 0; j < listElement1.length; j++) {
         if (listElement2[i] === listElement1[j]) {
-            found = true;
+            is_false = true;
             break;
         }
     }
-    if (!found) uniqueListEl2.push(listElement2[i]);
+    if (!is_false) uniqueListEl2.push(listElement2[i]);
 }
 
 console.log(uniqueListEl2);
